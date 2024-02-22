2016f0d27eb5ba1e8e946e06c68531388c49af19
Noortje Manintveld <nmanintveld@student.tudelft.nl>
2024-02-22 20:02:32 +0100

    Adding equals & hash method

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -1,12 +1,28 @@
 package commons;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Event {
     private ArrayList<Participant> Participants;
     private ArrayList<Expense> Expenses;
     private String name;
-    private String inventationID;
+    private String invitationID;
+
+    /**
+     * Constructor for an Event object
+     * @param participants ArrayList of participants which are part of the event
+     * @param expenses ArrayList of expenses that are part of the event
+     * @param name String with name of the event
+     * @param invitationID String with invitation ID of the event
+     */
+    public Event(ArrayList<Participant> participants, ArrayList<Expense> expenses, String name,
+                 String invitationID) {
+        this.Participants = participants;
+        this.Expenses = expenses;
+        this.name = name;
+        this.invitationID = invitationID;
+    }
 
     public ArrayList<Participant> getParticipants() {
         return Participants;
@@ -21,7 +37,7 @@ public class Event {
     }
 
     public void removeParticipant(ArrayList<Participant> participants, Participant participant){
-        participants.remove(participant);b
+        participants.remove(participant);
     }
 
     public ArrayList<Expense> getExpenses() {
@@ -40,11 +56,27 @@ public class Event {
         this.name = name;
     }
 
-    public String getInventationID() {
-        return inventationID;
+    public String getInvitationID() {
+        return invitationID;
     }
 
-    public void setInventationID(String inventationID) {
-        this.inventationID = inventationID;
+    public void setInvitationID(String invitationID) {
+        this.invitationID = invitationID;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Event event = (Event) object;
+        return Objects.equals(Participants, event.Participants) &&
+            Objects.equals(Expenses, event.Expenses) &&
+            Objects.equals(name, event.name) &&
+            Objects.equals(invitationID, event.invitationID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Participants, Expenses, name, invitationID);
     }
 }
