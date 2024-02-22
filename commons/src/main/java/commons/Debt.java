437cfe004d4e0f3add77098cb27683ca96834c02
Aayush Gupta <a.gupta-58@student.tudelft.nl>
2024-02-22 19:32:15 +0100

    Refactored received into settled, constructor added

--- commons/src/main/java/commons/Debt.java
+++ commons/src/main/java/commons/Debt.java
@@ -3,17 +3,24 @@ package commons;
 import java.util.Objects;
 
 public class Debt {
-    private boolean received;
+    private boolean settled;
     private Participant from;
     private Participant to;
     private double value;
 
-    public boolean isReceived() {
-        return received;
+    public Debt(Participant from, Participant to, double value) {
+        this.from = from;
+        this.to = to;
+        this.value = value;
+        settled = false;
     }
 
-    public void setReceived(boolean received) {
-        this.received = received;
+    public boolean isSettled() {
+        return settled;
+    }
+
+    public void setSettled(boolean settled) {
+        this.settled = settled;
     }
 
     public Participant getFrom() {
@@ -45,11 +52,11 @@ public class Debt {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Debt debt = (Debt) o;
-        return received == debt.received && Double.compare(value, debt.value) == 0 && Objects.equals(from, debt.from) && Objects.equals(to, debt.to);
+        return settled == debt.settled && Double.compare(value, debt.value) == 0 && Objects.equals(from, debt.from) && Objects.equals(to, debt.to);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(received, from, to, value);
+        return Objects.hash(settled, from, to, value);
     }
 }
