68ea5291f2daf3b74bcc9799299e6e3451a53a88
Noortje Manintveld <nmanintveld@student.tudelft.nl>
2024-02-22 20:20:38 +0100

    Add and remove methods added plus comments

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Event {
-    private ArrayList<Participant> Participants;
-    private ArrayList<Expense> Expenses;
+    /**
+     * Arraylist with all the participants which are part of this event
+     */
+    private ArrayList<Participant> participants;
+    /**
+     * Arraylist with all the expenses which are part of this event
+     */
+    private ArrayList<Expense> expenses;
+    /**
+     * String with the name of the event
+     */
     private String name;
+    /**
+     * String with invitation ID, which the participants can invite others with
+     */
     private String invitationID;
 
     /**
@@ -18,65 +30,134 @@ public class Event {
      */
     public Event(ArrayList<Participant> participants, ArrayList<Expense> expenses, String name,
                  String invitationID) {
-        this.Participants = participants;
-        this.Expenses = expenses;
+        this.participants = participants;
+        this.expenses = expenses;
         this.name = name;
         this.invitationID = invitationID;
     }
 
+    /**
+     * Getter for the list of participants
+     * @return ArrayList with all participants
+     */
     public ArrayList<Participant> getParticipants() {
-        return Participants;
+        return participants;
     }
 
+    /**
+     * Getter for the list of participants
+     * @param participants ArrayList with all the participants
+     */
     public void setParticipants(ArrayList<Participant> participants) {
-        this.Participants = participants;
+        this.participants = participants;
     }
 
+    /**
+     * Add method to add a new participant to the list of participants of the event
+     * @param participants ArrayList with participants
+     * @param newParticipant Participant object that will be added to the list
+     */
     public void addParticipant(ArrayList<Participant> participants, Participant newParticipant){
         participants.add(newParticipant);
     }
 
+    /**
+     * Remove method to remove a participant from the list of participants of the event
+     * @param participants ArrayList with participants
+     * @param participant Participant object that will be added to the list
+     */
     public void removeParticipant(ArrayList<Participant> participants, Participant participant){
         participants.remove(participant);
     }
 
+    /**
+     * Getter for the list of expenses
+     * @return ArrayList with all the expenses of the event
+     */
     public ArrayList<Expense> getExpenses() {
-        return Expenses;
+        return expenses;
     }
 
+    /**
+     * Setter for the list of expenses
+     * @param expenses ArrayList with expenses of the event
+     */
     public void setExpenses(ArrayList<Expense> expenses) {
-        this.Expenses = expenses;
+        this.expenses = expenses;
     }
 
+    /**
+     * Add method to add a new expense to the list of expenses of the event
+     * @param expenses Arraylist with the expenses
+     * @param newExpense Expense object that will be added to the list
+     */
+    public void addExpense (ArrayList<Expense> expenses, Expense newExpense){
+        expenses.add(newExpense);
+    }
+
+    /**
+     * Remove method to remove an expense from the list of expenses of the event
+     * @param expenses ArrayList with expenses
+     * @param expense Expense object that will be removed
+     */
+    public void removeExpense(ArrayList<Expense> expenses, Expense expense){
+        expenses.remove(expense);
+    }
+
+    /**
+     * Getter for the name of the event
+     * @return String with name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter for the name of the event
+     * @param name String with name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter for the invitation ID of the event
+     * @return String with invitation ID
+     */
     public String getInvitationID() {
         return invitationID;
     }
 
+    /**
+     * Setter for the invitation ID of the event
+     * @param invitationID String with invitation ID
+     */
     public void setInvitationID(String invitationID) {
         this.invitationID = invitationID;
     }
 
+    /**
+     * Equals method for the Event class
+     * @param object Object that will be compared to
+     * @return boolean true or false if equal or not
+     */
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
         if (object == null || getClass() != object.getClass()) return false;
         Event event = (Event) object;
-        return Objects.equals(Participants, event.Participants) &&
-            Objects.equals(Expenses, event.Expenses) &&
+        return Objects.equals(participants, event.participants) &&
+            Objects.equals(expenses, event.expenses) &&
             Objects.equals(name, event.name) &&
             Objects.equals(invitationID, event.invitationID);
     }
 
+    /**
+     * Hash method for the Event class
+     * @return Integer with hash code
+     */
     @Override
     public int hashCode() {
-        return Objects.hash(Participants, Expenses, name, invitationID);
+        return Objects.hash(participants, expenses, name, invitationID);
     }
 }
