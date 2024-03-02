27cd5f633b63738ed1b4e254b6ed0e65508cbd8f
Ege Yarar <e.yarar@student.tudelft.nl>
2024-03-02 18:32:38 +0300

    invitation

--- client/src/main/java/client/scenes/OpenDebtsCtrl.java
+++ client/src/main/java/client/scenes/OpenDebtsCtrl.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 
 import javax.inject.Inject;
 import java.util.Objects;
