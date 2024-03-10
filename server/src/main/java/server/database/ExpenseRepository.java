6099aa13e3c7025c35ab13410cf09df5e6c0d6cc
Filip Nedic <f.nedic@student.tudelft.nl>
2024-03-10 22:49:39 +0100

    ExpenseRepository useless import deleted

--- server/src/main/java/server/database/ExpenseRepository.java
+++ server/src/main/java/server/database/ExpenseRepository.java
@@ -1,7 +1,6 @@
 package server.database;
 
 import commons.Expense;
-import commons.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
