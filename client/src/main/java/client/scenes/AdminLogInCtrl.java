3d1097ad07a774d26241d60629645985e686b1d2
Noortje Manintveld <n.j.manintveld@student.tudelft.nl>
2024-03-10 14:24:03 +0100

    checkstyle

--- client/src/main/java/client/scenes/AdminLogInCtrl.java
+++ client/src/main/java/client/scenes/AdminLogInCtrl.java
@@ -2,7 +2,7 @@ package client.scenes;
 
 import client.utils.RefServerUtils;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+//import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
