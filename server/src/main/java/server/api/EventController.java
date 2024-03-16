66a053f80517e25d268337162830d9b2a777ebe5
Ege Yarar <e.yarar@student.tudelft.nl>
2024-03-16 16:31:57 +0300

    eventctrl

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -14,16 +14,29 @@ public class EventController {
         this.db=db;
     }
 
-    @GetMapping("/event/{id}")
-    @ResponseBody
-    public String id(@PathVariable("id") Long id){
-        if(!db.existsById(id)){
-            var event=new Event();
-            event.setName("name");
-            db.save(event);
+    @PutMapping("/event/{invitationID}")
+    public ResponseEntity<Event> updateEvent(@RequestBody Event event, @PathVariable("invitationID") String event_id) {
+        long id=Long.parseLong(event_id);
+        if(event == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (!db.existsById(id)){
+            return ResponseEntity.notFound().build();
         }
 
-        return "Hello " + id + "!";
+        Event existingEvent = db.findById(id).get();
+        existingEvent.setParticipants(event.getParticipants());
+        existingEvent.setExpenses(event.getExpenses());
+        existingEvent.setName(event.getName());
+        Event updatedEventEntity = db.save(existingEvent);
+        return ResponseEntity.ok(updatedEventEntity);
+    }
+
+    @DeleteMapping("/event/{invitationID}")
+    public ResponseEntity<Event> deleteEvent(@PathVariable("invitationID") String event_id) {
+        long id=Long.parseLong(event_id);
+        db.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/event")
@@ -38,5 +51,15 @@ public class EventController {
         Event event=db.findById(id).get();
         return ResponseEntity.ok(event);
     }
+
+    @PostMapping()
+    public ResponseEntity<Event> createEvent(@RequestBody Event event) {
+        if (event == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        Event createdEvent = db.save(event);
+        return ResponseEntity.ok(createdEvent);
+    }
+
 
 }
