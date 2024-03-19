abba315065305559164ce1c0aaa81d0ce4b3feda
Noortje Manintveld <n.j.manintveld@student.tudelft.nl>
2024-03-19 15:43:38 +0100

    Revert "some refactoring"
    This reverts commit 57160f5871b3c3d3e78e019c8056fcaef33d2b15.

--- server/src/test/java/server/api/ExpenseControllerTest.java
+++ server/src/test/java/server/api/ExpenseControllerTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import server.ExpenseController;
 import server.database.ExpenseRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
