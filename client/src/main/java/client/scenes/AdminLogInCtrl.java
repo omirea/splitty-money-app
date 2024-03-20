0f217d6754ef15de6cd3470c4538b9cb9bd514ef
Noortje Manintveld <n.j.manintveld@student.tudelft.nl>
2024-03-20 10:27:35 +0100

    Admin log in method fixed

--- client/src/main/java/client/scenes/AdminLogInCtrl.java
+++ client/src/main/java/client/scenes/AdminLogInCtrl.java
@@ -3,6 +3,7 @@ package client.scenes;
 import client.utils.RefServerUtils;
 import javafx.fxml.FXML;
 //import javafx.scene.control.Alert;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -14,26 +15,13 @@ public class AdminLogInCtrl {
 
     private final RefServerUtils server;
     private final MainCtrl mainCtrl;
-    @FXML
-    private VBox vboxContainer;
 
     @FXML
-    private TextField username;
+    private TextField textUsername;
 
     @FXML
-    private PasswordField password;
+    private PasswordField textPassword;
 
-    @FXML
-    private Text textUsername;
-
-    @FXML
-    private Text textPassword;
-
-    @FXML
-    private Button homeButton;
-
-    @FXML
-    private Button logInButton;
 
     @Inject
     public AdminLogInCtrl(RefServerUtils server, MainCtrl mainCtrl) {
@@ -46,9 +34,10 @@ public class AdminLogInCtrl {
      * @return true or false if login was successful
      */
     public boolean onClickLogIn(){
-        if(checkPassword() && checkEmptyFields() && checkUsername() ){
+        if(checkEmptyFields() && checkPassword() && checkUsername() ){
             mainCtrl.showEventsAdmin();
-            // TODO: connect to admin overview events;
+            textPassword.setText("");
+            textUsername.setText("");
             return true;
         } else return false;
     }
@@ -65,21 +54,19 @@ public class AdminLogInCtrl {
      * @return true if the fields are both filled, else false
      */
     public boolean checkEmptyFields(){
-        //TODO: doesnt work yet, need to figure out why
-//        boolean usernameField = username.getText().trim().isEmpty();
-//        boolean passwordField = password.getText().trim().isEmpty();
-//
-//        if(!(usernameField || passwordField)){
-//            return true;
-//        } else {
-//            Alert alertEmpty = new Alert(Alert.AlertType.WARNING);
-//            alertEmpty.setTitle("Empty Field(s)");
-//            alertEmpty.setHeaderText(null);
-//            alertEmpty.setContentText("Please Fill In All The Fields");
-//            alertEmpty.showAndWait();
-//            return false;
-//        }
-        return true;
+        boolean usernameField = textUsername.getText().trim().isEmpty();
+        boolean passwordField = textPassword.getText().trim().isEmpty();
+
+        if(!(usernameField || passwordField)){
+            return true;
+        } else {
+            Alert alertEmpty = new Alert(Alert.AlertType.WARNING);
+            alertEmpty.setTitle("Empty Field(s)");
+            alertEmpty.setHeaderText(null);
+            alertEmpty.setContentText("Please Fill In All The Fields");
+            alertEmpty.showAndWait();
+            return false;
+        }
     }
 
     /**
@@ -87,20 +74,19 @@ public class AdminLogInCtrl {
      * @return true if the password is correct, else false
      */
     public boolean checkPassword(){
-        //TODO: doesnt work yet, need to figure out why
-//        String passwordAdmin = "AdminPassword123!";
-//        String passwordFromField = password.getText().trim();
-//        if(passwordFromField.equals(passwordAdmin)){
-//            return true;
-//        } else {
-//            Alert alertPassword = new Alert(Alert.AlertType.WARNING);
-//            alertPassword.setTitle("Password is incorrect");
-//            alertPassword.setHeaderText(null);
-//            alertPassword.setContentText("Try password again");
-//            alertPassword.showAndWait();
-//            return false;
-//        }
-        return true;
+        //TODO: Figure out how to do specific username/password combinations
+        String passwordAdmin = "AdminPassword123!";
+        String passwordFromField = textPassword.getText().trim();
+        if(passwordFromField.equals(passwordAdmin)){
+            return true;
+        } else {
+            Alert alertPassword = new Alert(Alert.AlertType.WARNING);
+            alertPassword.setTitle("Password is incorrect");
+            alertPassword.setHeaderText(null);
+            alertPassword.setContentText("Try password again");
+            alertPassword.showAndWait();
+            return false;
+        }
     }
 
     /**
@@ -108,20 +94,20 @@ public class AdminLogInCtrl {
      * @return true if the username is correct, else false
      */
     public boolean checkUsername(){
-        //TODO: doesnt work yet, need to figure out why
-//        String usernameAdmin = "AdminSplitty";
-//        String usernameFromField = username.getText().trim();
-//        if(usernameAdmin.equals(usernameFromField)){
-//            return true;
-//        } else {
-//            Alert alertUsername = new Alert(Alert.AlertType.WARNING);
-//            alertUsername.setTitle("Username is incorrect");
-//            alertUsername.setHeaderText(null);
-//            alertUsername.setContentText("Try username again");
-//            alertUsername.showAndWait();
-//            return false;
-//        }
-        return true;
+        //TODO: Figure out how to do specific username/password combinations
+        String usernameAdmin = "AdminSplitty";
+        String usernameFromField = textUsername.getText().trim();
+        if(usernameAdmin.equals(usernameFromField)){
+            return true;
+        } else {
+            Alert alertUsername = new Alert(Alert.AlertType.WARNING);
+            alertUsername.setTitle("Username is incorrect");
+            alertUsername.setHeaderText(null);
+            alertUsername.setContentText("Try username again");
+            alertUsername.showAndWait();
+            return false;
+        }
+
     }
 
 }
