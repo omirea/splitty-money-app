f60bf07749b8e835ee537501b7922b88a1f6a63e
Ege Yarar <e.yarar@student.tudelft.nl>
2024-03-23 15:30:15 +0300

    event server utils

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -15,6 +15,7 @@
  */
 package client.utils;
 
+import commons.Event;
 import commons.Participant;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
@@ -93,5 +94,71 @@ public class ServerUtils {
 				.accept(APPLICATION_JSON)
 				.put(Entity.entity(participant, APPLICATION_JSON),
 					Participant.class);
+	}
+
+	/**
+	 *
+	 * @return a list of events
+	 */
+	public List<Event> getAllEvents(){
+		return ClientBuilder.newClient(new ClientConfig())
+				.target(SERVER).path("/event")
+				.request(APPLICATION_JSON)
+				.accept(APPLICATION_JSON)
+				.get(new GenericType<List<Event>>() {});
+	}
+
+	/**
+	 * method to get a specific Event based on invitationID
+	 * @param invitationID id of the Event requested
+	 * @return the requested Event
+	 */
+	public Event getEventById(String invitationID){
+		return ClientBuilder.newClient(new ClientConfig())
+				.target(SERVER).path("/event/" + invitationID)
+				.request(APPLICATION_JSON)
+				.accept(APPLICATION_JSON)
+				.get(new GenericType<Event>() {});
+	}
+
+	/**
+	 * method to delete a specific Event  from the database
+	 * @param invitationID the id of the Event  to be deleted
+	 * @return the deleted Event
+	 */
+	public Event deleteEvent (String invitationID){
+		return ClientBuilder.newClient(new ClientConfig())
+				.target(SERVER).path("/event/" + invitationID)
+				.request(APPLICATION_JSON)
+				.accept(APPLICATION_JSON)
+				.delete(new GenericType<Event>() {});
+	}
+
+	/**
+	 * method to create a new Event  in the database
+	 * @param event  Event  to be added to the database
+	 * @return the created Event
+	 */
+	public Event createEvent(Event event ){
+		return ClientBuilder.newClient(new ClientConfig())
+				.target(SERVER).path("/event")
+				.request(APPLICATION_JSON)
+				.accept(APPLICATION_JSON)
+				.post(Entity.entity(event , APPLICATION_JSON),
+						Event.class);
+	}
+
+	/**
+	 * method to update an Event  from the database
+	 * @param event Event  to be updated in the database
+	 * @return the updated Event
+	 */
+	public Event updateEvent(Event event , String invitationID){
+		return ClientBuilder.newClient(new ClientConfig())
+				.target(SERVER).path("/event/" + invitationID)
+				.request(APPLICATION_JSON)
+				.accept(APPLICATION_JSON)
+				.put(Entity.entity(event, APPLICATION_JSON),
+						Event.class);
 	}
 }
