1140cfa8fbe041103bcc22b249431bdaf65f961a
Aayush Gupta <a.gupta-58@student.tudelft.nl>
2024-03-23 14:34:52 +0100

    removed unused imports in debt controller

--- server/src/main/java/server/api/DebtController.java
+++ server/src/main/java/server/api/DebtController.java
@@ -1,8 +1,6 @@
 package server.api;
 
 import commons.Debt;
-import commons.Expense;
-import commons.Participant;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
