33ae3bba023622302e6cdd36585ce291e9f51834
Noortje Manintveld <n.j.manintveld@student.tudelft.nl>
2024-03-24 14:49:19 +0100

    added more test to participantCtrl

--- server/src/test/java/server/api/ParticipantControllerTest.java
+++ server/src/test/java/server/api/ParticipantControllerTest.java
@@ -1,5 +1,6 @@
 package server.api;
 
+import commons.Expense;
 import commons.Participant;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +10,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import server.database.ParticipantRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -38,13 +42,14 @@ public class ParticipantControllerTest {
     }
 
     @Test
-    public void getParticipantByIdNotExistTest(){
+    public void getParticipantByIdNegativeID(){
         ResponseEntity<Object> responseEntity=participantController.getParticipantByID((long)-1);
         assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
         assertNull(responseEntity.getBody());
     }
+
     @Test
-    public void postParticipantTest(){
+    public void postParticipantTest(){ //add to db
         Participant participant=new Participant();
         when(participantRepository.save(participant)).thenReturn(participant);
         ResponseEntity<Participant> responseEntity=participantController.createParticipant(participant);
@@ -53,7 +58,7 @@ public class ParticipantControllerTest {
     }
 
     @Test
-    public void postNullParticipantTest(){
+    public void postNullParticipantTest(){ //test to add to db
         ResponseEntity<Participant> responseEntity=participantController.createParticipant(null);
         assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
         assertNull(responseEntity.getBody());
@@ -78,10 +83,68 @@ public class ParticipantControllerTest {
     }
 
     @Test
-    public void deleteExpenseTest() {
+    public void testUpdateParticipantNull() {
+        long id = 100;
+        ResponseEntity<Participant> responseEntity = participantController
+            .updateParticipant(null, id);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertNull(responseEntity.getBody());
+    }
+
+    @Test
+    public void testUpdateParticipantNotFound() {
+        long id = 150;
+        Participant participant = new Participant();
+        when(participantRepository.existsById(id)).thenReturn(false);
+        ResponseEntity<Participant> responseEntity = participantController
+            .updateParticipant(participant, id);
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        assertNull(responseEntity.getBody());
+    }
+
+    @Test
+    public void testUpdateParticipantNegativeID() {
+        long id = -1;
+        Participant participant = new Participant();
+        ResponseEntity<Participant> responseEntity = participantController
+            .updateParticipant(participant, id);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertNull(responseEntity.getBody());
+    }
+
+    @Test
+    public void deleteParticipantTest() {
         long participantId = 1;
+        when(participantRepository.existsById(participantId)).thenReturn(true);
         ResponseEntity<Participant> responseEntity = participantController.deleteParticipant(participantId);
         assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
         verify(participantRepository, times(1)).deleteById(participantId);
+    }
+
+    @Test
+    public void testDeleteParticipantNotFound() {
+        long id = 150;
+        when(participantRepository.existsById(id)).thenReturn(false);
+        ResponseEntity<Participant> responseEntity = participantController.deleteParticipant(id);
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        assertNull(responseEntity.getBody());
+    }
+
+    @Test
+    public void testDeleteParticipantNegativeID() {
+        long id = -1;
+        ResponseEntity<Participant> responseEntity = participantController.deleteParticipant(id);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertNull(responseEntity.getBody());
+    }
+
+
+
+    @Test
+    public void testGetAllParticipants() {
+        List<Participant> dummy = new ArrayList<>();
+        when(participantRepository.findAll()).thenReturn(dummy);
+        List<Participant> listOfParticipants = participantController.getAllParticipants();
+        assertEquals(listOfParticipants, dummy);
     }
 }
