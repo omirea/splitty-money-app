8964b1ca0f1ade9881cc1f7d557460718d7fb7e7
Ege Yarar <e.yarar@student.tudelft.nl>
2024-03-24 20:22:04 +0300

    eventtest

--- server/src/test/java/server/api/TestExpenseRepository.java
+++ server/src/test/java/server/api/TestExpenseRepository.java
@@ -49,8 +49,10 @@ public class TestExpenseRepository implements ExpenseRepository {
     }
 
     /**
-     * Deletes the given entities in a batch which means it will create a single query. This kind of operation leaves JPAs
-     * first level cache and the database out of sync. Consider flushing the {@link EntityManager} before calling this
+     * Deletes the given entities in a batch which means it will create a single query.
+     * This kind of operation leaves JPAs
+     * first level cache and the database out of sync.
+     * Consider flushing the {@link EntityManager} before calling this
      * method.
      * <p>
      * It will also NOT honor cascade semantics of JPA, nor will it emit JPA  lifecycle events.
@@ -65,8 +67,10 @@ public class TestExpenseRepository implements ExpenseRepository {
     }
 
     /**
-     * Deletes the entities identified by the given ids using a single query. This kind of operation leaves JPAs first
-     * level cache and the database out of sync. Consider flushing the {@link EntityManager} before calling this method.
+     * Deletes the entities identified by the given ids using a single query.
+     * This kind of operation leaves JPAs first
+     * level cache and the database out of sync.
+     * Consider flushing the {@link EntityManager} before calling this method.
      *
      * @param longs the ids of the entities to be deleted. Must not be {@literal null}.
      * @since 2.5
@@ -105,9 +109,11 @@ public class TestExpenseRepository implements ExpenseRepository {
     }
 
     /**
-     * Returns a reference to the entity with the given identifier. Depending on how the JPA persistence provider is
+     * Returns a reference to the entity with the given identifier.
+     * Depending on how the JPA persistence provider is
      * implemented this is very likely to always return an instance and throw an
-     * {@link EntityNotFoundException} on first access. Some of them will reject invalid identifiers
+     * {@link EntityNotFoundException} on first access.
+     * Some of them will reject invalid identifiers
      * immediately.
      *
      * @param aLong must not be {@literal null}.
@@ -120,11 +126,14 @@ public class TestExpenseRepository implements ExpenseRepository {
     }
 
     /**
-     * Returns a single entity matching the given {@link Example} or {@link Optional#empty()} if none was found.
+     * Returns a single entity matching the given {@link Example}
+     * or {@link Optional#empty()} if none was found.
      *
      * @param example must not be {@literal null}.
-     * @return a single entity matching the given {@link Example} or {@link Optional#empty()} if none was found.
-     * @throws IncorrectResultSizeDataAccessException if the Example yields more than one result.
+     * @return a single entity matching the given {@link Example}
+     * or {@link Optional#empty()} if none was found.
+     * @throws IncorrectResultSizeDataAccessException
+     * if the Example yields more than one result.
      */
     @Override
     public <S extends Expense> Optional<S> findOne(Example<S> example) {
@@ -143,34 +152,40 @@ public class TestExpenseRepository implements ExpenseRepository {
 
     /**
      * @param example must not be {@literal null}.
-     * @param sort    the {@link Sort} specification to sort the results by, may be {@link Sort#unsorted()}, must not be
+     * @param sort    the {@link Sort} specification to sort the results by,
+     *                may be {@link Sort#unsorted()}, must not be
      *                {@literal null}.
      * @param <S>
      * @return
      */
     @Override
-    public <S extends Expense> List<S> findAll(Example<S> example, Sort sort) {
+    public <S extends Expense> List<S> findAll(Example<S> example,
+                                               Sort sort) {
         return null;
     }
 
     /**
-     * Returns a {@link Page} of entities matching the given {@link Example}. In case no match could be found, an empty
+     * Returns a {@link Page} of entities matching the given {@link Example}.
+     * In case no match could be found, an empty
      * {@link Page} is returned.
      *
      * @param example  must not be {@literal null}.
-     * @param pageable the pageable to request a paged result, can be {@link Pageable#unpaged()}, must not be
+     * @param pageable the pageable to request a paged result,
+     *                 can be {@link Pageable#unpaged()}, must not be
      *                 {@literal null}.
      * @return a {@link Page} of entities matching the given {@link Example}.
      */
     @Override
-    public <S extends Expense> Page<S> findAll(Example<S> example, Pageable pageable) {
+    public <S extends Expense> Page<S> findAll(Example<S> example,
+                                               Pageable pageable) {
         return null;
     }
 
     /**
      * Returns the number of instances matching the given {@link Example}.
      *
-     * @param example the {@link Example} to count instances for. Must not be {@literal null}.
+     * @param example the {@link Example} to count instances for.
+     *                Must not be {@literal null}.
      * @return the number of instances matching the {@link Example}.
      */
     @Override
@@ -181,8 +196,10 @@ public class TestExpenseRepository implements ExpenseRepository {
     /**
      * Checks whether the data store contains elements that match the given {@link Example}.
      *
-     * @param example the {@link Example} to use for the existence check. Must not be {@literal null}.
-     * @return {@literal true} if the data store contains elements that match the given {@link Example}.
+     * @param example the {@link Example} to use for the existence check.
+     *                Must not be {@literal null}.
+     * @return {@literal true} if the data store contains
+     * elements that match the given {@link Example}.
      */
     @Override
     public <S extends Expense> boolean exists(Example<S> example) {
@@ -190,28 +207,37 @@ public class TestExpenseRepository implements ExpenseRepository {
     }
 
     /**
-     * Returns entities matching the given {@link Example} applying the {@link Function queryFunction} that defines the
+     * Returns entities matching the given {@link Example}
+     * applying the {@link Function queryFunction} that defines the
      * query and its result type.
      *
      * @param example       must not be {@literal null}.
-     * @param queryFunction the query function defining projection, sorting, and the result type
+     * @param queryFunction the query function defining projection,
+     *                     sorting, and the result type
      * @return all entities matching the given {@link Example}.
      * @since 2.6
      */
     @Override
-    public <S extends Expense, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+    public <S extends Expense, R> R findBy(Example<S> example,
+                                           Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
         return null;
     }
 
     /**
-     * Saves a given entity. Use the returned instance for further operations as the save operation might have changed the
+     * Saves a given entity.
+     * Use the returned instance for further operations
+     * as the save operation might have changed the
      * entity instance completely.
      *
      * @param entity must not be {@literal null}.
      * @return the saved entity; will never be {@literal null}.
-     * @throws IllegalArgumentException          in case the given {@literal entity} is {@literal null}.
-     * @throws OptimisticLockingFailureException when the entity uses optimistic locking and has a version attribute with
-     *                                           a different value from that found in the persistence store. Also thrown if the entity is assumed to be
+     * @throws IllegalArgumentException
+     * in case the given {@literal entity} is {@literal null}.
+     * @throws OptimisticLockingFailureException
+     * when the entity uses optimistic locking and has a version attribute with
+     *
+     *                                          a different value from that found in the persistence store.
+     *                                          Also thrown if the entity is assumed to be
      *                                           present but does not exist in the database.
      */
     @Override
@@ -223,13 +249,18 @@ public class TestExpenseRepository implements ExpenseRepository {
      * Saves all given entities.
      *
      * @param entities must not be {@literal null} nor must it contain {@literal null}.
-     * @return the saved entities; will never be {@literal null}. The returned {@literal Iterable} will have the same size
+     * @return the saved entities; will never be {@literal null}. T
+     * he returned {@literal Iterable} will have the same size
      * as the {@literal Iterable} passed as an argument.
-     * @throws IllegalArgumentException          in case the given {@link Iterable entities} or one of its entities is
+     * @throws IllegalArgumentException
+     * in case the given {@link Iterable entities} or one of its entities is
      *                                           {@literal null}.
-     * @throws OptimisticLockingFailureException when at least one entity uses optimistic locking and has a version
-     *                                           attribute with a different value from that found in the persistence store. Also thrown if at least one
-     *                                           entity is assumed to be present but does not exist in the database.
+     * @throws OptimisticLockingFailureException
+     * when at least one entity uses optimistic locking and has a version
+     *
+     *  attribute with a different value from that found in the persistence store.
+     *  Also thrown if at least one
+     * entity is assumed to be present but does not exist in the database.
      */
     @Override
     public <S extends Expense> List<S> saveAll(Iterable<S> entities) {
@@ -240,7 +271,8 @@ public class TestExpenseRepository implements ExpenseRepository {
      * Retrieves an entity by its id.
      *
      * @param aLong must not be {@literal null}.
-     * @return the entity with the given id or {@literal Optional#empty()} if none found.
+     * @return the entity with the given id or
+     * {@literal Optional#empty()} if none found.
      * @throws IllegalArgumentException if {@literal id} is {@literal null}.
      */
     @Override
@@ -252,7 +284,8 @@ public class TestExpenseRepository implements ExpenseRepository {
      * Returns whether an entity with the given id exists.
      *
      * @param aLong must not be {@literal null}.
-     * @return {@literal true} if an entity with the given id exists, {@literal false} otherwise.
+     * @return {@literal true} if an entity with the given id exists,
+     * {@literal false} otherwise.
      * @throws IllegalArgumentException if {@literal id} is {@literal null}.
      */
     @Override
@@ -278,9 +311,11 @@ public class TestExpenseRepository implements ExpenseRepository {
      * Note that the order of elements in the result is not guaranteed.
      *
      * @param longs must not be {@literal null} nor contain any {@literal null} values.
-     * @return guaranteed to be not {@literal null}. The size can be equal or less than the number of given
+     * @return guaranteed to be not {@literal null}.
+     * The size can be equal or less than the number of given
      * {@literal ids}.
-     * @throws IllegalArgumentException in case the given {@link Iterable ids} or one of its items is {@literal null}.
+     * @throws IllegalArgumentException in case the given
+     * {@link Iterable ids} or one of its items is {@literal null}.
      */
     @Override
     public List<Expense> findAllById(Iterable<Long> longs) {
@@ -300,10 +335,12 @@ public class TestExpenseRepository implements ExpenseRepository {
     /**
      * Deletes the entity with the given id.
      * <p>
-     * If the entity is not found in the persistence store it is silently ignored.
+     * If the entity is not found in the persistence
+     * store it is silently ignored.
      *
      * @param aLong must not be {@literal null}.
-     * @throws IllegalArgumentException in case the given {@literal id} is {@literal null}
+     * @throws IllegalArgumentException in case the given
+     * {@literal id} is {@literal null}
      */
     @Override
     public void deleteById(Long aLong) {
@@ -314,10 +351,13 @@ public class TestExpenseRepository implements ExpenseRepository {
      * Deletes a given entity.
      *
      * @param entity must not be {@literal null}.
-     * @throws IllegalArgumentException          in case the given entity is {@literal null}.
-     * @throws OptimisticLockingFailureException when the entity uses optimistic locking and has a version attribute with
-     *                                           a different value from that found in the persistence store. Also thrown if the entity is assumed to be
-     *                                           present but does not exist in the database.
+     * @throws IllegalArgumentException
+     * in case the given entity is {@literal null}.
+     * @throws OptimisticLockingFailureException
+     * when the entity uses optimistic locking and has a version attribute with
+     * a different value from that found in the persistence store.
+     * Also thrown if the entity is assumed to be
+     *  present but does not exist in the database.
      */
     @Override
     public void delete(Expense entity) {
@@ -329,8 +369,10 @@ public class TestExpenseRepository implements ExpenseRepository {
      * <p>
      * Entities that aren't found in the persistence store are silently ignored.
      *
-     * @param longs must not be {@literal null}. Must not contain {@literal null} elements.
-     * @throws IllegalArgumentException in case the given {@literal ids} or one of its elements is {@literal null}.
+     * @param longs must not be {@literal null}.
+     *             Must not contain {@literal null} elements.
+     * @throws IllegalArgumentException in case the given
+     * {@literal ids} or one of its elements is {@literal null}.
      * @since 2.5
      */
     @Override
@@ -341,11 +383,15 @@ public class TestExpenseRepository implements ExpenseRepository {
     /**
      * Deletes the given entities.
      *
-     * @param entities must not be {@literal null}. Must not contain {@literal null} elements.
-     * @throws IllegalArgumentException          in case the given {@literal entities} or one of its entities is {@literal null}.
-     * @throws OptimisticLockingFailureException when at least one entity uses optimistic locking and has a version
-     *                                           attribute with a different value from that found in the persistence store. Also thrown if at least one
-     *                                           entity is assumed to be present but does not exist in the database.
+     * @param entities must not be {@literal null}.
+     * Must not contain {@literal null} elements.
+     * @throws IllegalArgumentException
+     * in case the given {@literal entities} or one of its entities is {@literal null}.
+     * @throws OptimisticLockingFailureException
+     * when at least one entity uses optimistic locking and has a version
+     * attribute with a different value from that found in the persistence store.
+     * Also thrown if at least one
+     * entity is assumed to be present but does not exist in the database.
      */
     @Override
     public void deleteAll(Iterable<? extends Expense> entities) {
@@ -363,7 +409,8 @@ public class TestExpenseRepository implements ExpenseRepository {
     /**
      * Returns all entities sorted by the given options.
      *
-     * @param sort the {@link Sort} specification to sort the results by, can be {@link Sort#unsorted()}, must not be
+     * @param sort the {@link Sort} specification to sort the results by,
+     * can be {@link Sort#unsorted()}, must not be
      *             {@literal null}.
      * @return all entities sorted by the given options
      */
@@ -373,10 +420,12 @@ public class TestExpenseRepository implements ExpenseRepository {
     }
 
     /**
-     * Returns a {@link Page} of entities meeting the paging restriction provided in the {@link Pageable} object.
+     * Returns a {@link Page} of entities meeting the paging restriction
+     * provided in the {@link Pageable} object.
      *
-     * @param pageable the pageable to request a paged result, can be {@link Pageable#unpaged()}, must not be
-     *                 {@literal null}.
+     * @param pageable the pageable to request a paged result,
+     * can be {@link Pageable#unpaged()}, must not be
+     *   {@literal null}.
      * @return a page of entities
      */
     @Override
