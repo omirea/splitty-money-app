b428a05b742cc07dec3ef30b4a58d922de0f2cf5
Noe Narayudha Purwanto <n.n.purwanto@student.tudelft.nl>
2024-03-27 15:55:15 +0100

    fixed button import

--- client/src/main/java/client/scenes/StartCtrl.java
+++ client/src/main/java/client/scenes/StartCtrl.java
@@ -6,6 +6,7 @@ import com.google.inject.Inject;
 import commons.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
