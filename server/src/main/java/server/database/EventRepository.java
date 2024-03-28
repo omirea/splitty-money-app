4df7bafea76b7add9a97869cbdda40c5119bf091
Noortje Manintveld <n.j.manintveld@student.tudelft.nl>
2024-03-28 14:16:20 +0100

    deleted unused files

--- server/src/main/java/server/database/EventRepository.java
+++ server/src/main/java/server/database/EventRepository.java
@@ -2,11 +2,6 @@ package server.database;
 
 import commons.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.http.ResponseEntity;
-
-import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
