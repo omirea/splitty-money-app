050e1e06f8cb9e904803641560e8be02ada9d411
Noortje Manintveld <n.j.manintveld@student.tudelft.nl>
2024-04-01 16:20:41 +0200

    changed delete button to trash can (easier bc of translate stuff)

--- client/src/main/java/client/nodes/RecentEvent.java
+++ client/src/main/java/client/nodes/RecentEvent.java
@@ -3,11 +3,16 @@ package client.nodes;
 import client.scenes.MainCtrl;
 import commons.Event;
 import jakarta.inject.Inject;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+
+import java.util.Objects;
 
 public class RecentEvent {
     HBox hbox;
@@ -40,15 +45,45 @@ public class RecentEvent {
         link = new Button("Open ");
         // might need to put the url from event into the param
         link.setOnAction(e -> openRecentEvent());
-        remove = new Button("Remove ");
+        remove = new Button();
+        ImageView imageView = new ImageView();
+        imageView.setFitHeight(20);
+        imageView.setFitWidth(20);
+        Image trash =new Image(Objects.requireNonNull
+            (getClass().getResourceAsStream("/icons/trash.png")));
+        imageView.setImage(trash);
+        remove.setGraphic(imageView);
         remove.setOnAction(e -> removeRecentEvent());
         hbox = new HBox();
         hbox.getChildren().add(new Text(this.event.getName() +"  "));
         hbox.getChildren().add(link);
         hbox.getChildren().add(remove);
+        hbox.setMargin(remove, new Insets(0, 0, 0, 10));
         hbox.setAlignment(Pos.CENTER_LEFT);
 
     }
+
+//
+//
+//
+//
+//        switch (locale.getLanguage()){
+//            case "nl": link = new Button("Open ");
+//                link.setOnAction(e -> openRecentEvent());
+//                remove = new Button("Verwijder ");
+//                remove.setOnAction(e -> removeRecentEvent());
+//                break;
+//            case "en":
+//                link = new Button("Open ");
+//                link.setOnAction(e -> openRecentEvent());
+//                remove = new Button("Remove ");
+//                remove.setOnAction(e -> removeRecentEvent());
+//                break;
+//            default: break;
+
+
+
+
 
 
     public HBox getNode(){
