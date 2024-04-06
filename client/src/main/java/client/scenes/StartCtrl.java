f3ab9558225f45622ff9f2a1e40ba91ffc3b1aaf
Noortje Manintveld <n.j.manintveld@student.tudelft.nl>
2024-04-06 21:10:36 +0200

    Revert "Merge branch 'editmanageparticipant' into 'main'"
    This reverts merge request !114

--- client/src/main/java/client/scenes/StartCtrl.java
+++ client/src/main/java/client/scenes/StartCtrl.java
@@ -50,14 +50,11 @@ public class StartCtrl implements  Main.LanguageSwitch {
     @FXML
     private TableColumn<Event, Button> openColumn;
 
-
-
     @Inject
     public StartCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
     }
-
 
     /**
      * method to initialize the page view (table, settings icon and admin icon)
@@ -110,7 +107,6 @@ public class StartCtrl implements  Main.LanguageSwitch {
             });
             return row;
         });
-        recentEvents.refresh();
     }
 
     /**
@@ -180,7 +176,6 @@ public class StartCtrl implements  Main.LanguageSwitch {
      */
     public void addEventToBox(Event event) {
         recentEvents.getItems().add(event);
-        //recentEvents.refresh();
     }
 
     private void showEvent(String invID) {
