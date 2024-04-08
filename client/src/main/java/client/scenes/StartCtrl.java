6b6151cacb998733f804637f5d0d111b1a605d56
Noortje Manintveld <n.j.manintveld@student.tudelft.nl>
2024-04-08 17:43:39 +0200

    clearing fields

--- client/src/main/java/client/scenes/StartCtrl.java
+++ client/src/main/java/client/scenes/StartCtrl.java
@@ -142,6 +142,7 @@ public class StartCtrl implements  Main.LanguageSwitch {
             e = server.createEvent(e);
             mainCtrl.showEventOverview(e.getInvitationID());
             createEventField.setText("");
+            joinEventField.setText(" ");
         } else {
             Alert alertNameEmpty = getAlertNameEmpty();
             alertNameEmpty.showAndWait();
@@ -177,6 +178,7 @@ public class StartCtrl implements  Main.LanguageSwitch {
             Event e =  server.getEventByInvitationId(joinEventField.getText());
             addEventToBox(e);
             joinEventField.setText("");
+            createEventField.setText(" ");
         } catch (Exception e) {
             Alert alert = getAlertIncorrectInvitationId();
             alert.show();
